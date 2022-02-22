2f836626b4c058791567765d1d92756e99ab33cd
isveny <amineprojet7@gmail.com>
2022-02-22 14:27:48 +0100

    cleanup

--- pages/games.tsx
+++ pages/games.tsx
@@ -59,14 +59,14 @@ const MyGames = () => {
           </div>
         </div>
       </div>
-      <div className="fixed bottom-0 right-0 px-5 py-5">
+      <FadeIn className="fixed bottom-0 right-0 px-5 py-5">
         <button
           onClick={() => scroll.scrollToTop()}
           className="bg-[#6444a4] px-2 py-2 rounded-full transition transform hover:-translate-y-3 shadow-xl"
         >
           <Icons name="arrow-top" className="w-6 h-6 text-white" />
         </button>
-      </div>
+      </FadeIn>
     </>
   );
 };
