8eb80b038498d668b284ecd1860fb5fde59ffd32
amineohn <amineprojet7@gmail.com>
2022-02-24 14:48:36 +0100

    better responsives grids.

--- pages/games.tsx
+++ pages/games.tsx
@@ -24,15 +24,15 @@ const MyGames = () => {
   return (
     <>
       <Navigation />
-      <div className="mt-2 w-full container m-auto space-y-6 py-4 lg:px-0 px-4">
-        <div className="flex flex-col space-y-3 justify-center items-center">
-          <div className="grid lg:grid-cols-4 xl:grid-cols-5 grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-2">
+      <div className="container px-4 py-4 m-auto mt-2 space-y-6 w-full lg:px-0">
+        <div className="flex flex-col justify-center items-center space-y-3">
+          <div className="grid grid-cols-1 gap-2 lg:grid-cols-4 xl:grid-cols-6 md:grid-cols-3 sm:grid-cols-2">
             {data ? (
               <>
                 {data.map((item: Games) => (
                   <>
                     <FadeIn className="space-y-4">
-                      <div className="px-2 py-2 rounded-xl bg-white font-medium">
+                      <div className="px-2 py-2 font-medium bg-white rounded-xl">
                         <p>{item.name}</p>
                       </div>
                       <img
@@ -59,7 +59,7 @@ const MyGames = () => {
           </div>
         </div>
       </div>
-      <FadeIn className="fixed bottom-0 right-0 px-5 py-5">
+      <FadeIn className="fixed right-0 bottom-0 px-5 py-5">
         <button
           onClick={() => scroll.scrollToTop()}
           className="bg-[#6444a4] px-2 py-2 rounded-full transition transform hover:-translate-y-3 shadow-xl"
